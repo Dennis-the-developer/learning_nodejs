7f13845c13a9f35f120c088ea303cf240655286d
dennis-the-developer <dev.dennisappah@gmail.com>
2024-06-20 13:22:05 +0000

    File system in nodejs

--- server.mjs
+++ server.mjs
@@ -1,11 +1,28 @@
-import {createServer} from 'node:http';
+import { createServer } from 'node:http';
+import { unlink, writeFile } from 'node:fs';
 
 // Create HTTP Server
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>We have an HTTP Server</h1>');
-}
-)
+    console.log(req.url);
+    if (req.url.includes('create')) {
+        // create file
+        writeFile('./abc.html', '<h1>Learning Node.js</h1>', (err) => {
+            console.log(err);
+            // Return response
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>We have an HTTP Server</h1>');
+        });
+
+    } else {
+        // Delete file
+        unlink('./hello.html', () => {
+            // Return response
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h1>File deleted</h1>');
+        });
+    }
+
+});
 
 // Listen for incomming request
 server.listen(3000, '127.0.0.1', () => {
@@ -14,5 +31,5 @@ server.listen(3000, '127.0.0.1', () => {
 
 
 // Normal function vs Arrow Function
-function handleRequest(){}
-const handleRequest = () => {}
+// function handleRequest() { }
+// const handleRequest = () => { }
